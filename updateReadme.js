d78db1ffb0e9cfedd33ad0401ca21f5a73ae3f4c
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-12-16 16:31:45 -0500

    Update updateReadme.js

--- updateReadme.js
+++ updateReadme.js
@@ -1,6 +1,7 @@
 // Include node fs (file stream) and https modules
 const fs = require('fs');
 const https = require('https');
+const NotionAPI = require('notion-client')
 
 // API endpoint
 const url = 'https://dev.to/api/articles?username=<YOUR DEV USERNAME>';
@@ -50,5 +51,66 @@ function readWriteAsync() {
   });
 }
 
+
+const main = async () => {
+  // you can optionally pass an authToken to access private notion resources
+  const api = new NotionAPI.NotionAPI()
+
+  // fetch a page's content, including all async blocks, collection queries, and signed urls
+  const page = await api.getPage('a49c1e962b7646879176ac3b327b6533')
+  // console.log(JSON.stringify(page['block'], null, 2))
+
+  const blocks = page['block']
+  const keys = Object.keys(blocks)
+  const imageURLs = []
+  keys.forEach(key => {
+    const block = blocks[key]
+    if (block['value']['type'] === 'image') {
+      const image = block['value']['format']['display_source']
+      const url = new URL(
+        `https://www.notion.so${image.startsWith("/image") ? image : `/image/${encodeURIComponent(image)}`
+        }`
+      );
+
+      const table = block.value.parent_table === "space" ? "block" : block.value.parent_table;
+      url.searchParams.set("table", table);
+      url.searchParams.set("id", block.value.id);
+      url.searchParams.set("cache", "v2");
+
+
+      imageURLs.push(url.toString())
+    }
+  })
+
+  const randomImage = imageURLs[Math.floor(Math.random() * imageURLs.length)]
+
+  console.log(randomImage)
+  
+  fs.readFile('README.md', 'utf-8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        // Replace text using regex: "I'm writing: ...replace... ![Build"
+        // Regex101.com is a lifesaver!
+        const updatedMd = data.replace(
+          /meme/gim,
+          randomImage
+        );
+
+        // Write the new README
+        fs.writeFile('README.md', updatedMd, 'utf-8', (err) => {
+          if (err) { 
+            throw err;
+          }
+
+          console.log('README update complete.');
+        });
+      });
+  
+  return randomImage
+
+}
+
 // Call the function
-readWriteAsync();
+main();
